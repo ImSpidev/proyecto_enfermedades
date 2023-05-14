884cfd566f05a881d2d83f350db2e92f5659454e
Charly <spidev.cto@gmail.com>
2023-05-14 15:06:26 -0600

    First Commit

---
+++ Conexion.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 25 13:24:11 2023
+
+@author: a
+"""
+
+import pyodbc
+
+
+server =  'localhost'
+database = 'enfermedades_respiratorias'
+username = 'root'
+password = '' 
+
+
+try:
+   conexion = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
+except Exception as conexion:
+   print("oc",conexion)
+
+
+
+
+
